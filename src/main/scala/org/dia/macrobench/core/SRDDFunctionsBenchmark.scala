a3b34bec28021c2e681e514022508f55a7a1a74b
rpalamut <rahulpalamut@gmail.com>
2016-09-08 01:07:55 -0700

    reduce iteration count

--- src/main/scala/org/dia/macrobench/core/SRDDFunctionsBenchmark.scala
+++ src/main/scala/org/dia/macrobench/core/SRDDFunctionsBenchmark.scala
@@ -29,7 +29,7 @@ import org.dia.core.SRDDFunctions._
 @BenchmarkMode(Array(Mode.AverageTime))
 @OutputTimeUnit(TimeUnit.SECONDS)
 @Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
 @State(Scope.Thread)
 class SRDDFunctionsBenchmark {
